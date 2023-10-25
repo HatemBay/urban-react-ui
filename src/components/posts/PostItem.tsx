cccef7d5325623e0ebd13372ee21cbfe35f54315
hatemBay <bayoudhhatem5@gmail.com>
2023-10-25 18:58:23 +0100

    Add components for posts display

--- src/components/posts/PostItem.tsx
+++ src/components/posts/PostItem.tsx
@@ -10,70 +10,107 @@ import {
   HStack,
   Button,
   Icon,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { IoMdThumbsDown, IoMdThumbsUp } from "react-icons/io";
+import formatDate from "../../utils/formatDate";
 interface Props {
   post: Post;
 }
 
 const PostItem = ({ post }: Props) => {
+  const TextColor = useColorModeValue("gray.800", "white");
+  const BgColor = useColorModeValue("white", "gray.800");
+
   return (
     <Flex
       boxShadow="md"
       p={5}
-      bg="white"
+      bg={BgColor}
       borderRadius="lg"
-      maxWidth="xl"
+      maxWidth="2xl"
       w="100%"
     >
-      <Avatar size="lg" />
-      <VStack>
-        <Text textTransform="capitalize" size="4xl">
-          {post.title}
-        </Text>
-        <Text>{post.content}</Text>
-        <Text>{post.created_at}</Text>
-        <HStack spacing={0}>
-          <Button
-            display="inline"
-            role="button"
-            border="1px"
-            borderColor="black"
-            width="10px"
-            px={6}
-            py={2}
-            borderRightRadius="none"
-            borderLeftRadius="3xl"
-            borderBottom="2px"
+      <HStack spacing={10}>
+        <Avatar size="lg" />
+        <Flex flexDirection="column" textAlign="left" color={TextColor}>
+          <Heading
+            mb={3}
+            fontWeight="bold"
+            fontFamily="lora"
+            color="blue"
+            textTransform="capitalize"
+            fontSize="3xl"
           >
-            <HStack spacing={2}>
-              <Icon fontSize="15px" ml={-3} as={IoMdThumbsUp}></Icon>
-              <Text fontSize="10px" fontWeight="bold">
-                {post.likes_count}
-              </Text>
-            </HStack>
-          </Button>
-          <Button
-            role="button"
-            border="1px"
-            borderColor="black"
-            width="10px"
-            px={6}
-            py={2}
-            borderRightRadius="3xl"
-            borderLeftRadius="none"
-            borderBottom="2px"
-            textAlign="center"
+            {post.title}
+          </Heading>
+          <Text
+            mb={2}
+            fontFamily="Source Sans Pro"
+            fontWeight="normal"
+            fontSize="1.125rem"
           >
-            <HStack spacing={2}>
-              <Icon fontSize="15px" ml={-3} as={IoMdThumbsDown}></Icon>
-              <Text fontSize="10px" fontWeight="bold">
-                {post.dislikes_count}
-              </Text>
-            </HStack>
-          </Button>
-        </HStack>
-      </VStack>
+            A mysterious word which is actually made up of the abbreviation “ur”
+            and the word “ban.” It probably hints to how the government is
+            planning to ban Urban Dictionary because it speaks the truth about
+            English and they’re jealous their dictionary can’t be the same.
+            {post.content}
+          </Text>
+          <Text mb={3} fontWeight="bold" fontSize="sm">
+            by{" "}
+            <Text as="span" color="blue">
+              {post.author.username}
+            </Text>{" "}
+            {formatDate(post.created_at)}
+            {/* {month} {date}, {year} */}
+            {/* //TODO: check time formatting after using data from db */}
+          </Text>
+          <HStack spacing={0} w="100%" h="100%">
+            <Button
+              display="inline"
+              role="button"
+              border="1px"
+              borderColor={TextColor}
+              width="70px"
+              px={6}
+              py={2}
+              borderRightRadius="none"
+              borderLeftRadius="full"
+              borderBottom="2px"
+              background={BgColor}
+              _hover={{ background: "green" }}
+            >
+              <HStack spacing={2}>
+                <Icon fontSize="15px" ml={-3} as={IoMdThumbsUp}></Icon>
+                <Text fontSize="10px" fontWeight="bold">
+                  {post.likes_count}
+                </Text>
+              </HStack>
+            </Button>
+            <Button
+              role="button"
+              border="1px"
+              borderColor={TextColor}
+              width="70px"
+              px={6}
+              py={2}
+              borderRightRadius="full"
+              borderLeftRadius="none"
+              borderBottom="2px"
+              textAlign="center"
+              background={BgColor}
+              _hover={{ background: "green" }}
+            >
+              <HStack spacing={2}>
+                <Icon fontSize="15px" ml={-3} as={IoMdThumbsDown}></Icon>
+                <Text fontSize="10px" fontWeight="bold">
+                  {post.dislikes_count}
+                </Text>
+              </HStack>
+            </Button>
+          </HStack>
+        </Flex>
+      </HStack>
     </Flex>
   );
 };
--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -1,42 +1,61 @@
-import { Box, Heading, VStack } from "@chakra-ui/react";
-import React from "react";
+import { Box, Heading, VStack, useColorModeValue } from "@chakra-ui/react";
+import React, { useState } from "react";
 import PostItem from "./PostItem";
+import { gql, useQuery } from "urql";
+import { Post } from "../../types";
+
+const PostsQuery = gql`
+  query posts($orderBy: OrderByParams) {
+    posts(orderBy: $orderBy) {
+      id
+      author {
+        username
+      }
+      author_id
+      title
+      content
+      published
+      is_u_18
+      likes_count
+      dislikes_count
+      created_at
+      updated_at
+    }
+  }
+`;
+
+type PostsQueryRes = {
+  posts: Post[];
+};
 
 type Props = {};
 
 export const Posts = (props: Props) => {
+  const [field, setOrderByField] = useState("created_at");
+
+  const BgColor = useColorModeValue("gray.800", "white");
+
+  const [{ data, fetching, error }] = useQuery<PostsQueryRes>({
+    query: PostsQuery,
+    variables: {
+      orderBy: {
+        field,
+        direction: "desc",
+      },
+    },
+  });
+
+  if (error) return <p>Something went wrong...</p>;
+  if (fetching || !data) return <p>Loading...</p>;
+
   return (
-    <Box w="100%">
+    <Box w="100%" color={BgColor}>
       <Heading textTransform="capitalize">posts</Heading>
 
       <VStack spacing={4}>
-        <PostItem
-          post={{
-            title: "title",
-            content: "lorem ipsum dolor",
-            likes_count: 5,
-            dislikes_count: 0,
-            created_at: "2024-10-19T16:26:10.174Z",
-          }}
-        ></PostItem>
-        <PostItem
-          post={{
-            title: "title",
-            content: "lorem ipsum dolor",
-            likes_count: 5,
-            dislikes_count: 0,
-            created_at: "2024-10-19T16:26:10.174Z",
-          }}
-        ></PostItem>
-        <PostItem
-          post={{
-            title: "title",
-            content: "lorem ipsum dolor",
-            likes_count: 5,
-            dislikes_count: 0,
-            created_at: "2024-10-19T16:26:10.174Z",
-          }}
-        ></PostItem>
+        {data.posts.map((post) => (
+          <PostItem post={post}></PostItem>
+        ))}
       </VStack>
     </Box>
   );
