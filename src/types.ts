6affdc9192fc6c7b03e4b2f92d6e8ad07abd7e6d
hatemBay <bayoudhhatem5@gmail.com>
2023-10-25 18:59:26 +0100

    Add data shapes for users and posts

--- src/types.ts
+++ src/types.ts
@@ -3,11 +3,26 @@ export interface Post {
   title: string;
   content: string;
   author_id?: number;
+  author: User;
   published?: boolean;
   is_u_18?: boolean;
   email_verified?: boolean;
   likes_count?: number;
   dislikes_count?: number;
+  created_at: string;
+  updated_at?: string;
+}
+
+export interface User {
+  id?: number;
+  username: string;
+  email: string;
+  name?: string;
+  country_id?: number;
+  role?: string;
+  is_u_18?: boolean;
+  email_verified?: boolean;
+  google_id?: string;
   created_at: string;
   updated_at?: string;
 }
