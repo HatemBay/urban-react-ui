5f7041477f23ddda07ffec611bc2ea21819bedb1
unknown <bayoudhhatem5@gmail.com>
2023-11-06 18:21:59 +0100

    Add constants file

---
+++ src/utils/constants/constants.ts
@@ -0,0 +1,7 @@
+import { globalColors } from "../interfaces/colors";
+
+export const SHARED_COLORS: globalColors = {
+    PrimaryBgColor: { light: "white", dark: "gray.800" },
+    SecondaryBgColor: { light: "gray.300", dark: "gray.600" },
+    TextColor: { light: "gray.800", dark: "white" }
+}
