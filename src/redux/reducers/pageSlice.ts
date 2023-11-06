ceeaa25670cfa40cb1901a91fa62b8e775f81b89
unknown <bayoudhhatem5@gmail.com>
2023-11-06 18:23:57 +0100

    Change page state name to currPage for more description

--- src/redux/reducers/pageSlice.ts
+++ src/redux/reducers/pageSlice.ts
@@ -1,11 +1,11 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit"
 
 export interface PageState {
-  page: number
+  currPage: number
 }
 
 const initialState: PageState = {
-  page: 1,
+  currPage: 1,
 }
 
 const pageSlice = createSlice({
@@ -13,23 +13,23 @@ const pageSlice = createSlice({
   initialState,
   reducers: {
     setPage: (state, action: PayloadAction<number>) => {
-      state.page = action.payload
+      state.currPage = action.payload
     },
     incremented: state => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.page += 1
+      state.currPage += 1
     },
     decremented: state => {
-      state.page -= 1
+      state.currPage -= 1
     }
   }
 })
 
 export const { setPage, incremented, decremented } = pageSlice.actions
 
-export const selectPage = (state: PageState) => state.page
+export const selectPage = (state: PageState) => state.currPage
 
 export default pageSlice.reducer;
