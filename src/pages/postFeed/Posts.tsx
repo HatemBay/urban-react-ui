ada2db50f1e6f1fdcc798c3ae429ce665b6fa328
unknown <bayoudhhatem5@gmail.com>
2023-11-30 14:54:50 +0100

    Add randomize feature to get random posts

--- src/pages/postFeed/Posts.tsx
+++ src/pages/postFeed/Posts.tsx
@@ -1,8 +1,8 @@
-import { Box, Heading, Select, VStack } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, Heading, Select, VStack } from "@chakra-ui/react";
+import React, { useEffect } from "react";
 import PostItem from "./PostItem";
 import { useQuery } from "urql";
-import { PaginatedPosts, Post } from "../../data/types";
+import { PaginatedPosts } from "../../data/types";
 import PageNavigator from "./PageNavigator";
 import { RootState } from "../../redux/store";
 import { useSelector } from "react-redux";
@@ -18,10 +18,11 @@ export const Posts = (props: Props) => {
   const TextColor = useLightDark(SHARED_COLORS.TextColor);
 
   const [field, setOrderByField] = React.useState("createdAt");
-  const [take, setTake] = React.useState(5);
+  let [take, setTake] = React.useState(5);
 
   const { currPage: page } = useSelector((state: RootState) => state.page)
   let { filter } = useSelector((state: RootState) => state.page)
+  let { randomize } = useSelector((state: RootState) => state.page)
 
   const filterActions: { [key: string]: () => void } = {
     "#": () => {
@@ -41,12 +42,16 @@ export const Posts = (props: Props) => {
     setTake(+e.target.value);
   };
 
+  const handleRandomPosts = () => {
+    reexecuteQuery({ requestPolicy: 'network-only' });
+    return window.scrollTo(0, 0);
+  };
 
   const changeField = (e: any) => {
     setOrderByField(() => "title");
   };
 
-  const [{ data, fetching, error }] = useQuery<PaginatedPosts>({
+  const [result, reexecuteQuery] = useQuery<PaginatedPosts>({
     query: POSTS_QUERY,
     variables: {
       orderBy: {
@@ -58,21 +63,22 @@ export const Posts = (props: Props) => {
         page,
         take,
       },
+      randomize
     },
   });
 
-  if (error) return <p> Something went wrong... </p>
+  const { data, fetching, error } = result;
+
+  if (error) return <p> Something went wrong... {error.name} </p>
   if (fetching || !data) return <p>Loading...</p>;
   return (
     <Box w="100%" color={TextColor}>
       <Heading textTransform="capitalize" mb={4}>
-        {data.posts.pagination.totalCount === 0 && "No "}
-
-        posts
+        {(data.posts.pagination !== undefined) && (data.posts.pagination.totalCount === 0) && "No "} posts
       </Heading>
       {/* <Button onClick={changeField}>Change</Button> */}
 
-      {data.posts.pagination.totalCount !== 0 && <VStack minH={"2xl"} spacing={4}>
+      {data.posts.pagination?.totalCount !== 0 && <VStack minH={"2xl"} spacing={4}>
         <Select
           onChange={changeTake}
           value={take}
@@ -92,7 +98,8 @@ export const Posts = (props: Props) => {
         {data.posts.data && data.posts.data.map((post) => (
           <PostItem key={post.id} post={post}></PostItem>
         ))}
-        <PageNavigator take={take} totalCount={data.posts.pagination.totalCount} />
+        {!randomize && <PageNavigator take={take} totalCount={data.posts.pagination.totalCount} />}
+        {randomize && <Button onClick={handleRandomPosts}>More random definitions</Button>}
       </VStack>}
     </Box >
   );
