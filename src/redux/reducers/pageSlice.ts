03d4f067e11b57200edbf0fd0cac301de437a295
unknown <bayoudhhatem5@gmail.com>
2023-11-30 16:35:35 +0100

    Add rerender state to force rerender when needed (used to force useQuery to bypass cache)

--- src/redux/reducers/pageSlice.ts
+++ src/redux/reducers/pageSlice.ts
@@ -4,12 +4,14 @@ export interface PageState {
   currPage: number,
   filter: string,
   randomize: boolean,
+  rerender: boolean,
 }
 
 const initialState: PageState = {
   currPage: 1,
   filter: "",
   randomize: false,
+  rerender: false,
 }
 
 const pageSlice = createSlice({
@@ -17,13 +19,13 @@ const pageSlice = createSlice({
   initialState,
   reducers: {
     setPage: (state, action: PayloadAction<number>) => {
-      state.currPage = action.payload
+      state.currPage = action.payload;
     },
     incremented: state => {
-      state.currPage += 1
+      state.currPage += 1;
     },
     decremented: state => {
-      state.currPage -= 1
+      state.currPage -= 1;
     },
     setFilter: (state, action: PayloadAction<string>) => {
       state.filter = action.payload;
@@ -34,11 +36,12 @@ const pageSlice = createSlice({
     clear: state => {
       state.filter = "";
     },
+    forceRerender: (state) => {
+      state.rerender = !state.rerender;
+    }
   }
 })
 
-export const { setPage, incremented, decremented, setFilter, setRandomize, clear } = pageSlice.actions
-
-export const selectPage = (state: PageState) => state.currPage
+export const { setPage, incremented, decremented, setFilter, setRandomize, forceRerender, clear } = pageSlice.actions
 
 export default pageSlice.reducer;
