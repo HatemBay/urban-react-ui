14526dadf4a7895ed59696a6005ed5ddc0fd3c0e
hatemBay <bayoudhhatem5@gmail.com>
2023-12-04 07:26:08 +0100

    Add type to authenticatioin slice state

--- src/redux/reducers/authSlice.ts
+++ src/redux/reducers/authSlice.ts
@@ -1,29 +1,37 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { User } from "../../data/types";
 
-const initialState = {
+interface AuthState {
+    loading: boolean;
+    userInfo: User;
+    userToken: any;
+    error: any;
+    success: boolean;
+}
+
+const initialState: AuthState = {
     loading: false,
     userInfo: {} as User, // for user object
     userToken: null, // for storing the JWT
     error: null,
     success: false, // for monitoring the registration process.
-}
+};
 
 //TODO: improve
 const authSlice = createSlice({
-    name: 'auth',
+    name: "auth",
     initialState,
     reducers: {
         setUserToken: (state, action: PayloadAction<any>) => {
-            state.userToken = action.payload
+            state.userToken = action.payload;
         },
         setUserInfo: (state, action: PayloadAction<any>) => {
-            state.userInfo = action.payload
+            state.userInfo = action.payload;
         },
     },
     extraReducers: {},
-})
+});
 
-export const { setUserToken, setUserInfo } = authSlice.actions
+export const { setUserToken, setUserInfo } = authSlice.actions;
 
-export default authSlice.reducer
+export default authSlice.reducer;
