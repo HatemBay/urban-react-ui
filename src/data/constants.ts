37cc6c7b5d07da9189d3307bc3c207730948f9bc
hatemBay <bayoudhhatem5@gmail.com>
2023-12-11 19:27:38 +0100

    Add secondary button color

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,10 +1,12 @@
 import { GlobalColors } from "../utils/interfaces/globalColors";
 
+// TODO: rename the colors
 export const SHARED_COLORS: GlobalColors = {
     PrimaryBgColor: { light: "white", dark: "gray.800" },
     SecondaryBgColor: { light: "gray.300", dark: "gray.600" },
     TextColor: { light: "gray.800", dark: "white" },
     ButtonPrimary: { light: "#758BFD", dark: "#758BFD" },
+    ButtonSecondary: { light: "#4361FC", dark: "#4361FC" },
     // LikeButtonHover: { light: "#758BFD" , dark: "#758BFD" },
 }
 
