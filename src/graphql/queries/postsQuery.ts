bd0b8eab8f7eb028b984e033dc33e41000095b22
unknown <bayoudhhatem5@gmail.com>
2023-12-14 20:05:23 +0100

    Include find by author id in posts query

--- src/graphql/queries/postsQuery.ts
+++ src/graphql/queries/postsQuery.ts
@@ -1,35 +1,45 @@
 import { gql } from "urql";
 
 export const POSTS_QUERY = gql`
-query Posts($orderBy: OrderByParams, $pagination: PaginationParams, $randomize: Boolean) {
-  posts(orderBy: $orderBy, pagination: $pagination, randomize: $randomize) {
-    data {
-      id
-      author {
-        username
-      }
-      example {
+  query Posts(
+    $orderBy: OrderByParams
+    $pagination: PaginationParams
+    $authorId: Int
+    $randomize: Boolean
+  ) {
+    posts(
+      orderBy: $orderBy
+      pagination: $pagination
+      authorId: $authorId
+      randomize: $randomize
+    ) {
+      data {
         id
+        author {
+          username
+        }
+        example {
+          id
+          contentArabic
+          contentEnglish
+          contentFrench
+        }
+        authorId
+        titleArabic
+        titleLatin
         contentArabic
         contentEnglish
         contentFrench
+        published
+        isU18
+        likesCount
+        dislikesCount
+        createdAt
+        updatedAt
       }
-      authorId
-      titleArabic
-      titleLatin
-      contentArabic
-      contentEnglish
-      contentFrench
-      published
-      isU18
-      likesCount
-      dislikesCount
-      createdAt
-      updatedAt
-    }
-    pagination {
-      totalCount
+      pagination {
+        totalCount
+      }
     }
   }
-}
 `;
