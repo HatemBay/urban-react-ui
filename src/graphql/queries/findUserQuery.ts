5b0f70c195aa08a5dce7dc72d17bceeee40ac2d9
HatemBay <bayoudhhatem5@gmail.com>
2024-02-08 18:45:06 +0100

    Add query for finding user

---
+++ src/graphql/queries/findUserQuery.ts
@@ -0,0 +1,15 @@
+import { gql } from "urql";
+
+export const FIND_USER_QUERY = gql`
+  query User($findUserInput: FindUserInput!) {
+    user(findUserInput: $findUserInput) {
+      id
+      username
+      email
+      emailVerified
+      name
+      isU18
+      countryId
+    }
+  }
+`;
